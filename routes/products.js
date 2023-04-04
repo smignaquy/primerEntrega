92086b59bc42e832c731b54f41dc4814d3f306e5
BautyRemondino2 <bremondino@udesa.edu.ar>
2023-04-04 14:06:18 -0300

    Header Logueado y perfiles

--- routes/products.js
+++ routes/products.js
@@ -2,10 +2,12 @@ let express = require('express');
 let router = express.Router();
 let productController = require('../controllers/productController')
 
-router.get('/', productController.index);
+// router.get('/', productController.index);
 
 router.get('/todos', productController.todosProductos);
 
 router.get('/id/:id', productController.product);
+
+router.get('/?search=:busqueda', productController.search)
 
 module.exports = router;
