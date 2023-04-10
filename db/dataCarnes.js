e8ebeea0b4a44d3d0e16f51f446dfa4881e108bf
smignaquy <smignaquy@ydesa.edu.ar>
2023-04-10 15:44:33 -0300

    db

--- db/dataCarnes.js
+++ db/dataCarnes.js
@@ -12,6 +12,7 @@ let cortesCarnes = {
         nombre : 'Bola de lomo',
         descripcion : 'Se caracteriza por ser un corte tierno y económico, su uso mas popular es para milanesas.',
         imagen : '/images/products/bola-de-lomo.jpg',
+        fecha_carga: '2022-12-18',
         comentarios : [
             {nombre: 'Juan Perez',
             foto : '/images/users/perfil1.png',
@@ -48,6 +49,7 @@ let cortesCarnes = {
         nombre : 'Chorizo',
         descripcion : 'El chorizo se elabora con carne picada y grasa de cerdo, condimentada con pimentón y ajo, todo ello embutido en tripa natural.',
         imagen : '/images/products/chorizo.jpg',
+        fecha_carga: '2022-12-18',
         comentarios : [
             {nombre: 'Juan Perez',
             foto : '/images/users/perfil1.png',
@@ -84,6 +86,7 @@ let cortesCarnes = {
         nombre : 'Entraña',
         descripcion : 'La entraña es una pieza procedente de la parte interna de los costillares de nuestras terneras, para entendernos mejor, sería el músculo del diafragma, por lo que de cada ternera podrán sacarse como mucho dos piezas de esta fabulosa carne',
         imagen : '/images/products/entrania.jpg',
+        fecha_carga: '2022-12-18',
         comentarios : [
             {nombre: 'Juan Perez',
             foto : '/images/users/perfil1.png',
@@ -120,6 +123,7 @@ let cortesCarnes = {
         nombre : 'Picaña',
         descripcion : 'La picaña tiene forma triangular y se corta en filetes o en dados. Está cubierta por una capa de grasa blanca -también posee alguna infiltración- que es responsable, en parte, del sabor, la terneza y la jugosidad únicos que posee esta preciada pieza',
         imagen : '/images/products/picania.jpg',
+        fecha_carga: '2022-12-18',
         comentarios : [
             {nombre: 'Juan Perez',
             foto : '/images/users/perfil1.png',
@@ -156,6 +160,7 @@ let cortesCarnes = {
         nombre : 'Tira de Asado',
         descripcion : 'La tira de asado se obtiene con el corte transversal del costillar de la res. Se encuentra en medio de otros dos clásicos cortes: las costillas, del lado más cercano a la columna vertebral, y la popular falda, que utilizamos para deshebrar, y se ubica del lado más lejano.',
         imagen : '/images/products/tira-de-asado.jpg',
+        fecha_carga: '2022-12-18',
         comentarios : [
             {nombre: 'Juan Perez',
             foto : '/images/users/perfil1.png',
@@ -192,6 +197,7 @@ let cortesCarnes = {
         nombre : 'Morcilla',
         descripcion : 'La morcilla es un embutido sin carne, relleno principalmente con sangre coagulada, en su mayoría de cerdo. Es de color oscuro característico.',
         imagen : '/images/products/morcilla.jpg',
+        fecha_carga: '2022-12-18',
         comentarios : [
             {nombre: 'Juan Perez',
             foto : '/images/users/perfil1.png',
@@ -228,6 +234,7 @@ let cortesCarnes = {
         nombre : 'Bife de Chorizo',
         descripcion : 'El bife de chorizo es la costeleta sin hueso ubicada en la cara externa del lomo del animal. Al ser separado del hueso, antes de cortarse en bifes, tiene forma triangular y cilíndrica.',
         imagen : '/images/products/bife-de-chorizo.jpg',
+        fecha_carga: '2022-12-18',
         comentarios : [
             {nombre: 'Juan Perez',
             foto : '/images/users/perfil1.png',
@@ -264,6 +271,7 @@ let cortesCarnes = {
         nombre : 'Vacio',
         descripcion : 'El vacio se obtiene de la parte lateral del vacuno, junto a las costillas. Entre sus características, podemos decir que es una carne un poco más fibrosa que el resto de piezas. Debido a ello, su sabor es muy sabroso.',
         imagen : '/images/products/vacio.jpg',
+        fecha_carga: '2022-12-18',
         comentarios : [
             {nombre: 'Juan Perez',
             foto : '/images/users/perfil1.png',
